0ff123f3592617ded96cd19d5f9fa693cf445f22
Uttam Sonani <uttamsoftices@gmail.com>
2023-07-29 11:47:32 +0530

    fix cors file issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -18,10 +18,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
 	allow do
 		origins '*'
-		resource(
-			headers: :any,
-			expose: ['access-token', 'expiry', 'token-type', 'Authorization']
-			methods: [:get, :post, :patch, :put, :delete, :options, :show]
-		)
+		resource '*',
+		headers: :any,
+		expose: ['access-token', 'expiry', 'token-type', 'Authorization'],
+		methods: [:get, :post, :patch, :put, :delete, :options, :show]
 	end
 end
